877a0ef13d13fde45a08993f2451d799dffe13e8
Theophileaseh <theophilenwachan@gmail.com>
2022-10-21 15:38:48 +0100

    Fix linter error

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
   def index
     message = Message.all.sample
-    render json: { message: message }
+    render json: { message: }
   end
 end
